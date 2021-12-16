352fcd936b4274f5c110a183a99d483ec1b89c55
pongsakorn <78690934+pongsakornsutana@users.noreply.github.com>
2021-12-16 12:37:46 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Test99
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string select;
+            Console.Write("Input skill name:");
+            name = Console.ReadLine();
+            if (name != "?")
+            {
+                Console.WriteLine("Input skill name:" + name);
+                Console.Write("? (Y/N): ");
+                select = Console.ReadLine();
+                if ("Y" != "Input skill name:")
+                {
+                    Console.Write("Input skill name:" + name);
+
+                    Console.Write("Input skill name:", name);
+                    Console.Write("? (Y/N): ");
+                    select = Console.ReadLine();
+                    if ("Y" == "Input skill name:")
+                    {
+                        Console.WriteLine("Input skill name:"+name);
+
+                    }
+                    if ("N" == "Input skill name:")
+                    {
+                        Console.WriteLine("Input skill name:"+name);
+                    }
+                }
+            }
+            else {
+                Console.WriteLine("Add dependency for"+name);
+                Console.Write("? (Y/N): ");
+                select = Console.ReadLine();
+                if ("Y" == "Input skill name:")
+                {
+                    Console.WriteLine("Input skill name:");
+                    name = Console.ReadLine();
+                }
+                if ("N" == "Input skill name:")
+                {
+                    Console.WriteLine("Input skill name:" + name);
+                }
+            }
+        }
+      
+
+
+        }
+    }
+
